f355f89e458069986636232ed9e5c862f2259011
jangtjdgns <jangtjdgns0208@gmail.com>
2023-11-28 20:35:49 +0900

    member 테이블 생성, member join 기능 추가

--- src/com/koreaIT/example/JAM/App.java
+++ src/com/koreaIT/example/JAM/App.java
@@ -52,9 +52,8 @@ public class App {
 					int id = DBUtil.insert(conn, sql);
 
 					System.out.printf("%d번 게시물이 생성되었습니다\n", id);
+				}
 
-				} 
-				
 				// 목록
 				else if (cmd.equals("article list")) {
 
@@ -80,9 +79,8 @@ public class App {
 					for (Article article : articles) {
 						System.out.printf("%d	|	%s	|	%s\n", article.id, article.title, article.regDate);
 					}
+				}
 
-				} 
-				
 				// 수정
 				else if (cmd.startsWith("article modify ")) {
 					int id = Integer.parseInt(cmd.split(" ")[2]);
@@ -113,32 +111,32 @@ public class App {
 
 					System.out.printf("%d번 게시물이 수정되었습니다\n", id);
 
-				} 
-				
+				}
+
 				// 조회
 				else if (cmd.startsWith("article detail ")) {
 					int id = Integer.parseInt(cmd.split(" ")[2]);
-					
+
 					SecSql sql = SecSql.from("SELECT * FROM article");
 					sql.append("WHERE id = ?", id);
-					
+
 					Map<String, Object> articleMap = DBUtil.selectRow(conn, sql);
-					
-					if(articleMap.isEmpty()) {		// Map타입은 isEmpty() 메서드를 제공함
+
+					if (articleMap.isEmpty()) {
 						System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 						continue;
 					}
-					
+
 					Article article = new Article(articleMap);
-					
+
 					System.out.printf("== %d번 게시물 상세보기 ==\n", id);
 					System.out.println("번  호: " + article.id);
 					System.out.println("작성일: " + article.regDate);
 					System.out.println("수정일: " + article.updateDate);
 					System.out.println("제  목: " + article.title);
 					System.out.println("내  용: " + article.body);
-				} 
-				
+				}
+
 				// 삭제
 				else if (cmd.startsWith("article delete ")) {
 					int id = Integer.parseInt(cmd.split(" ")[2]);
@@ -152,16 +150,90 @@ public class App {
 						System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 						continue;
 					}
-					
+
 					System.out.println("== 게시물 삭제 ==");
-					
+
 					sql = SecSql.from("DELETE FROM article");
 					sql.append("WHERE id = ?", id);
 					DBUtil.delete(conn, sql);
-					
+
 					System.out.printf("%d번 게시물이 삭제되었습니다.\n", id);
-				} 
-				
+				}
+
+				// 회원가입
+				else if (cmd.equals("member join")) {
+					
+					System.out.println("== 회원가입 ==");
+					
+					String loginId = null;
+					while (true) {
+						System.out.printf("아이디: ");
+						loginId = sc.nextLine().trim();
+
+						if (loginId.length() == 0) {
+							System.out.println("아이디는 필수입력정보입니다.");
+							continue;
+						}
+						
+						SecSql sql = SecSql.from("SELECT COUNT(*) > 0 FROM `member`");
+						sql.append("WHERE loginId = ?", loginId);
+						
+						boolean isLoginIdDup = DBUtil.selectRowBooleanValue(conn, sql);
+						
+						if(isLoginIdDup) {
+							System.out.printf("%s은(는) 이미 사용중인 아이디입니다.\n", loginId);
+							continue;
+						}
+
+						System.out.printf("%s은(는) 사용가능한 아이디입니다.\n", loginId);
+						break;
+					}
+
+					String loginPw = null;
+					while (true) {
+						System.out.printf("비밀번호: ");
+						loginPw = sc.nextLine().trim();
+
+						if (loginPw.length() == 0) {
+							System.out.println("비밀번호는 필수입력정보입니다.");
+							continue;
+						}
+						
+						System.out.printf("비밀번호확인: ");
+						String loginPwChk = sc.nextLine().trim();
+						
+						if(!loginPw.equals(loginPwChk)) {
+							System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
+							continue;
+						}
+
+						break;
+					}
+
+					String name = null;
+					while (true) {
+						System.out.printf("이름: ");
+						name = sc.nextLine().trim();
+						
+						if (name.length() == 0) {
+							System.out.println("이름은 필수입력정보입니다.");
+							continue;
+						}
+						break;
+					}
+					
+					SecSql sql = SecSql.from("INSERT INTO `member`");
+					sql.append("SET regDate = NOW()");
+					sql.append(", updateDate = NOW()");
+					sql.append(", loginId = ?", loginId);
+					sql.append(", loginPw = ?", loginPw);
+					sql.append(", `name` = ?", name);
+					                        
+					DBUtil.insert(conn, sql);
+					
+					System.out.printf("%s 회원님이 가입되었습니다.", name);
+				}
+
 				else {
 					System.out.println("존재하지 않는 명령어 입니다");
 				}
